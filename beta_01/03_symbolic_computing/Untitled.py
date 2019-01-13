286fb8e5c16b2de76062dfcd5c4c77e2b14545e2
Thapana Berpan <thaberp@Thapanas-MacBook-Pro.local>
2019-01-13 13:11:18 -0800

    Symbolic

---
+++ beta_01/03_symbolic_computing/Untitled.py
@@ -0,0 +1,38 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import sympy
+
+
+# In[2]:
+
+
+sympy.init_printing()
+
+
+# In[3]:
+
+
+from sympy import I,pi,oo
+
+
+# In[4]:
+
+
+x = sympy.Symbol("x")
+
+
+# In[5]:
+
+
+y = sympy.Symbol("y", real=True)
+
+
+# In[6]:
+
+
+y.is_real
+
